d115cb99b2a444b9e29fa5d131cf6f770564daa2
Santosh <santosh.shrigadiwar@gmail.com>
2021-06-01 21:43:04 +0530

    Add comment

--- poll/urls.py
+++ poll/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     Santosh Shriga1
+    Santosh code
 """
+
 from django.contrib import admin
 from django.urls import path, include
 

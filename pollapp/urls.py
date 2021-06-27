69e2a4bb215d4a7a0e515a6bc35025e309e3fbfe
sampada shrigadiwar <shrigadiwar.sampada@gmail.com>
2021-06-27 19:52:10 +0530

    Update urls.py in pollapp

--- pollapp/urls.py
+++ pollapp/urls.py
@@ -5,7 +5,7 @@ from pollapp  import views as poll_views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('', views.Index, name = 'Index'),
+    path('kitchen/', views.Index, name = 'Index'),
     path('login/', views.login, name = 'login'),
     path('register/', views.register, name = 'register'),
 ]
